fb9f017d82791c6778c7e32db3533025f2c5c44b
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2020-09-27 00:37:39 +0200

    fix wait for it

--- scripts/wait_for_it.sh
+++ scripts/wait_for_it.sh
@@ -1,13 +1,14 @@
 #/bin/bash
 
-max_retry=6
-count=0
-until [ ! $(nc -z localhost 8080) ]; do
-   sleep 2
-   [ $count -eq $max_retry ] && echo "Failed all api checks!" && exit 1
-   echo "Trying again. Try #${count}"
-   ((count++))
+for i in {1..5}
+do
+   nc -z localhost 8080
+   if [ $? == 0 ]
+   then
+      echo "Connection ready"
+      exit 0
+   fi
+   echo "Check #${i} failed"
+   echo "Retry in 3s..."
+   sleep 3
 done
-
-echo "Health check ready"
-echo "API started"
