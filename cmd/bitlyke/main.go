a3d38349d43bb75e59be0a66835203aec0481585
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2021-05-25 14:19:21 +0200

    fix

--- cmd/bitlyke/main.go
+++ cmd/bitlyke/main.go
@@ -22,7 +22,7 @@ func (cv *V) Validate(i interface{}) error {
 func main() {
 	cfg, err := config.FromEnv()
 	if err != nil {
-		log.Fatalf("failed env 
+		log.Fatalf("failed env init: %v", err)
 	}
 
 	db, err := pg.New(cfg.Postgres)
